6feaa32d4f0d30cde32d7ad8b116446b81e69f26
gporpino <gporpino@gmail.com>
2023-02-02 14:16:03 -0500

    add commentary plugin

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -163,12 +163,13 @@ lvim.builtin.treesitter.highlight.enable = true
 -- }
 
 -- Additional Plugins
--- lvim.plugins = {
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+  { "tpope/vim-commentary" }
+  --     {
+  --       "folke/trouble.nvim",
+  --       cmd = "TroubleToggle",
+  --     },
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
